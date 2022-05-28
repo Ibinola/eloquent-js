275d6152b380e30cadd70d497d92f1382f2f3f2d
Ibinola <ibinolamichael1@gmai.com>
2022-05-28 09:17:02 +0100

    chessboard.js2

--- chap-2/chessboard.js
+++ chap-2/chessboard.js
@@ -1,5 +1,30 @@
-// 1. create a string that represents an 8 x 8 grid, using newline characters to seperate lines. 
-//2. at each position of the grid there is either a space or a "#' character. 
-// 2. define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height
+/* 1. Define a binding size of the board, and define a variable of an empty string " ", that stores a space or a hash or a new line.
+
+2. Set a loop inside of a loop to generate a two dimensional grid: An outer loop is for rows to set board height ("i") and the inner loop is for columns to set board width ("j").
+
+3. Start building the board line by line from left to right and top to bottom by checking the sum of the two counters ((i+j) % 2 == 0)
+
+4. If the sum of the two counters is even; add a space (" ") to the board. if it is not even; then add a hash ("#") character to the board.
+
+5. When the inner loop finished, add ("\n") character to the board to generate a new line and then move to the next row.
+
+6. Print the board
+*/
 
 
+let size = 8;
+let board = "";
+
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+        if ((i + j) % 2 == 0) {
+            board += " "
+        }
+        else {
+            board += "#"
+        }
+    }
+    board += "\n"
+}
+
+console.log(board);
